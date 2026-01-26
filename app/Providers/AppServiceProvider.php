52a2d021e1b472931fa4dd9b7c51e23fe95effc6
WalkerVz <ravilmuhammad987@gmail.com>
2026-01-26 10:51:49 +0700

    upadate baru

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,12 +6,21 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-
-    public function boot(): void
+    /**
+     * Register any application services.
+     */
+    public function register(): void
     {
-        if ($this->app->environment('production')) {
-            \URL::forceScheme('https');
-        }
+        //
     }
 
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+    
+    
 }
