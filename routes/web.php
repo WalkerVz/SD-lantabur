0b0fe02efd09baf83f27b03ce0090030e3b9d273
Kaisyarendika <tengkukaisyar13@gmail.com>
2026-01-26 10:51:41 +0700

    init-db

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Artisan;
 
 Route::view('/', 'home');
 Route::view('/about', 'about');
 Route::view('/contact', 'contact');
 Route::view('/staff', 'staff');
 Route::view('/news', 'news');
+Route::get('/init-db', function () {
+    try {
+        // Ini akan membuat file database sqlite di folder /tmp Vercel
+        Artisan::call('migrate:fresh --force');
+        return "Database SQLite berhasil dibuat! Silakan buka halaman utama.";
+    } catch (\Exception $e) {
+        return "Gagal membuat database: " . $e->getMessage();
+    }
+});
