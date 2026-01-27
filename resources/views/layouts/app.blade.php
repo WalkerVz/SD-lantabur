974eed095aad7c845492ad36be6ff13ef98154aa
Kaisyarendika <tengkukaisyar13@gmail.com>
2026-01-27 13:29:27 +0700

    perubahan app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
     <script src="https://cdn.tailwindcss.com"></script>
-    <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+    
 </head>
 <body class="font-sans">
 
